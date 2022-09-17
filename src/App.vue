10f4a4666dbca46fdd02f6534c900e078bd69a74
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-09-17 22:58:22 +0700

    feat: support web push notification

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,30 @@
 <script setup lang="ts">
 import { useAuth } from '@/services';
 import { useDark } from '@vueuse/core'
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, onMounted } from 'vue';
+import { getTokenFcm, requestPermission, messaging, onMessage } from '@/services/useFirebase';
+import { useToast } from 'vue-toastification';
 
-useDark();
+const toast = useToast();
+
+onMounted(() => {
+  onMessage(messaging, (payload) => {
+        const notif = payload.notification as any;
+        const notificationOptions = {
+            title: `${notif?.title}`,
+            body: notif.body,
+            image: notif.image || `https://res.cloudinary.com/ekosutrisno/image/upload/v1662785818/briix/notif_n0ogoj.jpg`,
+            icon: 'https://res.cloudinary.com/ekosutrisno/image/upload/v1662786263/briix/n_pyzbuz.png',
+        };
+        var n = new Notification(notificationOptions.title, notificationOptions);
+        toast.info(notificationOptions.title);
+        n.onshow;
+    });
+  useDark();
+  requestPermission();
+  getTokenFcm();
+})
+
 
 // Authentication checking point
 const authService = useAuth()
