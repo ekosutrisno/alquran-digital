3d993ac677673dcd9d6b1c9dc3c08140f01889af
ekosutrisno1 <ekosutrisno801@gmail.com>
2023-01-13 20:20:52 +0700

    config: add autocomplete .env file import meta

--- src/env.d.ts
+++ src/env.d.ts
@@ -11,3 +11,21 @@ declare module '@heroicons/vue/solid';
 declare module '@heroicons/vue/outline';
 declare module 'virtual:pwa-register';
 declare module 'amplitudejs';
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  readonly VITE_BASE_MEDIA_SERVER: string;
+  readonly VITE_BASE_FIREBASE_APIKEY: string;
+  readonly VITE_BASE_FIREBASE_AUTHDOMAIN: string;
+  readonly VITE_BASE_FIREBASE_PROJECTID: string;
+  readonly VITE_BASE_FIREBASE_STORAGEBUCKET: string;
+  readonly VITE_BASE_FIREBASE_MESSAGINGSENDERID: string;
+  readonly VITE_BASE_FIREBASE_APPID: string;
+  readonly VITE_BASE_FIREBASE_MEASUREMENTID: string;
+  readonly VITE_BASE_FIREBASE_VAPID: string;
+  readonly VITE_BASE_APP_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
